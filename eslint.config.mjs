fd2ef8cdf7378013a178e9209630a82a15ded5ea
RayhanLauzzadani <rlauzzadani@gmail.com>
2025-06-15 20:53:21 +0700

    .

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
