b6bda0e7865a6d8058ae6e0c49d439573eb8f6e4
RayhanLauzzadani <rlauzzadani@gmail.com>
2025-06-16 11:52:24 +0700

    fix: memperbaiki beberapa konfigurasi

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest/presets/default-esm', // preset khusus untuk ES Modules + TS
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json', useESM: true }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov', 'html'],
+  collectCoverageFrom: [
+    'components/**/*.{js,jsx,ts,tsx}',
+    'app/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+  coverageDirectory: 'coverage',
+};
+
+export default config;
